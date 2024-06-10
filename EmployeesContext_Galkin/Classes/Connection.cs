b5a07d3236b44789f54558200f5be89b9f00808c
Nikita <galkinnikita228@gmail.com>
2024-06-10 22:03:10 +0500

    Разработанные классы общего пользования. Galkin

---
+++ EmployeesContext_Galkin/Classes/Connection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeesContext_Galkin.Classes
+{
+    public class Connection
+    {
+        private static readonly string config = "server=student.permaviat.ru;Trusted_Connection=No;TrustServerCertificate=True;DataBase=base2_ISP_21_2_2;User=ISP_21_2_2;pwd=7N1aGL8up#";
+
+        public static SqlConnection OpenConnection()
+        {
+            SqlConnection connection = new SqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlDataReader Query(string SQL, out SqlConnection connection)
+        {
+            connection = OpenConnection();
+            return new SqlCommand(SQL, connection).ExecuteReader();
+        }
+        public static void CloseConnection(SqlConnection connection)
+        {
+            connection.Close();
+            SqlConnection.ClearPool(connection);
+        }
+    }
+}
